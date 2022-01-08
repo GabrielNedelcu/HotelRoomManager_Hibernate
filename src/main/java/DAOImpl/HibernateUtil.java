a68e67a582403ca469150246bd442e3bd250413a
gabriel.nedelcu <gabriel.nedelcu2000@gmail.com>
2022-01-07 22:47:48 -0800

    Modified some hibernate configuration files

--- src/main/java/DAOImpl/HibernateUtil.java
+++ src/main/java/DAOImpl/HibernateUtil.java
@@ -1,6 +1,11 @@
 package DAOImpl;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.osgi.service.blueprint.reflect.Metadata;
 import org.hibernate.SessionFactory;
 /**
 * Hibernate Utility class with a convenient method to get Session Factory
@@ -8,21 +13,22 @@ import org.hibernate.SessionFactory;
 *
 */
 public class HibernateUtil {
- private static final SessionFactory sessionFactory;
- 
- static {
- try {
- // Create the SessionFactory from standard (hibernate.cfg.xml) 
- // config file.
- sessionFactory = new Configuration().configure().buildSessionFactory();
- } catch (Throwable ex) {
- // Log the exception. 
- System.err.println("Initial SessionFactory creation failed." + ex);
- throw new ExceptionInInitializerError(ex);
- }
- }
- 
- public static SessionFactory getSessionFactory() {
- return sessionFactory;
- }
+	private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                    .configure("hibernate.cfg.xml").build();
+            org.hibernate.boot.Metadata metadata = new MetadataSources(standardRegistry).getMetadataBuilder().build();
+            return metadata.getSessionFactoryBuilder().build();
+
+        } catch (HibernateException he) {
+            System.out.println("Session Factory creation failure");
+            throw he;
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 }
